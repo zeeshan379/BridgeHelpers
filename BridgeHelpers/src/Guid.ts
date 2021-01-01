71c3e99d77d49e7af715241df126c82a59e1bb28
Zeeshan UlHaq <zeeshan.ulhaq@bentley.com>
2021-01-01 15:35:34 +0500

    Added unit tests for StringToGuid method

--- BridgeHelpers/src/Guid.ts
+++ BridgeHelpers/src/Guid.ts
@@ -1,6 +1,10 @@
 export class Guid{
 
     public static StringToGuid (anyString : string) : any {
+    if(anyString === null || anyString === undefined || anyString === ""){
+        throw new Error("Incorrect parameter. Please provide a valid string.");
+    }
+
     var crypto = require("crypto");
     const hexToUuid = require('hex-to-uuid');
     var md5 = crypto.createHash("md5");     
--- BridgeHelpers/src/Test/Guid.Test.ts
+++ BridgeHelpers/src/Test/Guid.Test.ts
@@ -1,2 +1,46 @@
 import {Guid} from '../Guid';
 import { assert, expect } from "chai";
+
+describe("Bridge Helpers: Guid Tests", () => {
+    before(async () => {
+        
+    });
+
+    it("Should generate a GUID for 'Hello World' string", async () => {
+        var guid = Guid.StringToGuid('Hello World');
+        var expected ="b10a8db1-64e0-7541-05b7-a99be72e3fe5";
+        assert.equal(guid.toString(), expected);
+
+    });
+
+    it("Should throw error on null or empty string parameter", async () => {
+        try{
+           Guid.StringToGuid(null);
+        }
+        catch(error){
+        assert.equal(error.message, "Incorrect parameter. Please provide a valid string.");
+        }
+
+        try{
+            Guid.StringToGuid("");
+         }
+         catch(error){
+         assert.equal(error.message, "Incorrect parameter. Please provide a valid string.");
+         }
+    });
+
+    it("Should throw error on undefined parameter", async () => {
+            try{
+               Guid.StringToGuid(undefined);
+            }
+            catch(error){
+            assert.equal(error.message, "Incorrect parameter. Please provide a valid string.");
+            }
+
+    });
+
+    after(async () => {
+        
+      });
+
+});
